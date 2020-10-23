631287665654ed64d187af6becf2b1813b198bcc
anthonyophernandez <anthonyophernandez@gmail.com>
2020-10-23 08:43:34 +0200

    Height, Bg & Text Color, Font Size & Weight

--- views/index.view.php
+++ views/index.view.php
@@ -5,45 +5,51 @@
   <title>Staff Panel</title>
   <link rel="stylesheet" href="/public/css/style.css">
 </head>
-<body class="w-full">
-  <h1 class="w-full">Staff Panel</h1>
+<body class="w-full h-screen bg-gray-300">
+
+  <header class="bg-indigo-900">
+    <h1 class="text-lg text-white p-2">Staff Panel</h1>
+  </header>
 
   <div class="w-full">
-    <div class="flex justify-between w-12/12">
-      <div class="w-2/12 p-1 p-2">Id</div>
-      <div class="w-3/12 p-1 p-2">Name</div>
-      <div class="w-3/12 p-1 p-2">Position</div>
-      <div class="w-2/12 p-1 p-2">Experience</div>
-      <div class="w-2/12 p-1 p-2">Status</div>
+    <div class="flex justify-between w-full">
+      <div class="text-indigo-900 font-bold text-sm w-2/12 p-2">Id</div>
+      <div class="text-indigo-900 font-bold text-sm w-3/12 p-2">Name</div>
+      <div class="text-indigo-900 font-bold text-sm w-3/12 p-2">Position</div>
+      <div class="text-indigo-900 font-bold text-sm w-2/12 p-2">Experience</div>
+      <div class="text-indigo-900 font-bold text-sm w-2/12 p-2">Status</div>
     </div>
-    <div class="w-12/12">
+    <div class="w-full bg-gray-200">
       <?php foreach ($employees as $employee) : ?>
         <div class="flex justify-between w-full">
-          <div class="w-2/12 p-2">
+          <div class="w-2/12 p-2 text-gray-800">
             <?= $employee->getId(); ?> 
           </div>
 
-          <div class="w-3/12 p-2">
+          <div class="w-3/12 p-2 text-gray-800">
             <?= $employee->getName(); ?>
           </div>
 
-          <div class="w-3/12 p-2">
+          <div class="w-3/12 p-2 text-gray-800">
             <?= $employee->getPosition(); ?>
           </div>
 
-          <div class="w-2/12 p-2">
+          <div class="w-2/12 p-2 text-gray-800">
             <?= $employee->getExperience(); ?>
           </div>
 
-          <div class="w-2/12 p-2">
+          <div class="w-2/12 p-2 text-gray-800">
             <?= $employee->getStatus(); ?>
           </div>
         </div>
       <?php endforeach; ?>
     </div>
   </div>
+  
+  <div class="bg-indigo-900">
+    <h2 class="text-lg text-white p-2">Add Employee</h2>
+  </div>
 
-  <h2 class="w-full">Add Employee</h2>
   <form class="w-64 p-2" method="POST" action="/employee">
     <div class="flex flex-column justify-between w-full my-1">
       <label class="w-full" for="name">Name:</label>
